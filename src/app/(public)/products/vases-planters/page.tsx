171a723422319de0a559ca7ef16cf5c90b872665
Nahidur Rahman <nahidur.rahman.cs@gmail.com>
2026-06-01 19:51:01 +0600

    Add Vases and Planters category page with metadata generation

---
+++ src/app/(public)/products/vases-planters/page.tsx
@@ -0,0 +1,5 @@
+import { createCategoryMetadata, createCategoryPage } from "../categoryPage";
+
+export const generateMetadata = createCategoryMetadata("VASES_AND_PLANTERS");
+
+export default createCategoryPage("VASES_AND_PLANTERS");
