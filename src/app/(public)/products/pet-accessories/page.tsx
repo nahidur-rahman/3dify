2d27d1bf89ffc1142ef4959ad977a6798b7a9874
Nahidur Rahman <nahidur.rahman.cs@gmail.com>
2026-06-02 18:14:24 +0600

    Add Pet Accessories category page with metadata generation

---
+++ src/app/(public)/products/pet-accessories/page.tsx
@@ -0,0 +1,5 @@
+import { createCategoryMetadata, createCategoryPage } from "../categoryPage";
+
+export const generateMetadata = createCategoryMetadata("PET_ACCESSORIES");
+
+export default createCategoryPage("PET_ACCESSORIES");
