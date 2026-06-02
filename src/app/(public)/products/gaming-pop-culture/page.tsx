20cb59333add867a6e78287c97e2a836785cfc53
Nahidur Rahman <nahidur.rahman.cs@gmail.com>
2026-06-02 18:13:58 +0600

    Add Gaming and Pop Culture category page with metadata generation

---
+++ src/app/(public)/products/gaming-pop-culture/page.tsx
@@ -0,0 +1,5 @@
+import { createCategoryMetadata, createCategoryPage } from "../categoryPage";
+
+export const generateMetadata = createCategoryMetadata("GAMING_AND_POP_CULTURE");
+
+export default createCategoryPage("GAMING_AND_POP_CULTURE");
