5f858fb9235a8917837ee55076baef03e3ef28c0
Nahidur Rahman <nahidur.rahman.cs@gmail.com>
2026-06-02 18:13:19 +0600

    Add Collectibles and Figures category page with metadata generation

---
+++ src/app/(public)/products/collectibles-figures/page.tsx
@@ -0,0 +1,5 @@
+import { createCategoryMetadata, createCategoryPage } from "../categoryPage";
+
+export const generateMetadata = createCategoryMetadata("COLLECTIBLES_AND_FIGURES");
+
+export default createCategoryPage("COLLECTIBLES_AND_FIGURES");
