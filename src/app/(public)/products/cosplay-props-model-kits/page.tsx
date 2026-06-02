b465d598c1972545ef766c05ce5b75484a33f3c7
Nahidur Rahman <nahidur.rahman.cs@gmail.com>
2026-06-02 18:13:29 +0600

    Add Cosplay Props and Model Kits category page with metadata generation

---
+++ src/app/(public)/products/cosplay-props-model-kits/page.tsx
@@ -0,0 +1,5 @@
+import { createCategoryMetadata, createCategoryPage } from "../categoryPage";
+
+export const generateMetadata = createCategoryMetadata("COSPLAY_PROPS_AND_MODEL_KITS");
+
+export default createCategoryPage("COSPLAY_PROPS_AND_MODEL_KITS");
