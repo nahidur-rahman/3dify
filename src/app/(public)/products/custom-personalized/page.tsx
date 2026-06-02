070c73c27295e16de950c74f5f035dc0abd74bd3
Nahidur Rahman <nahidur.rahman.cs@gmail.com>
2026-06-02 18:13:37 +0600

    Add Custom and Personalized category page with metadata generation

---
+++ src/app/(public)/products/custom-personalized/page.tsx
@@ -0,0 +1,5 @@
+import { createCategoryMetadata, createCategoryPage } from "../categoryPage";
+
+export const generateMetadata = createCategoryMetadata("CUSTOM_AND_PERSONALIZED");
+
+export default createCategoryPage("CUSTOM_AND_PERSONALIZED");
