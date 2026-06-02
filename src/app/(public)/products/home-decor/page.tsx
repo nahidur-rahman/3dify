47cd0ffeed335ad5a33edfa24e274a614a5c981b
Nahidur Rahman <nahidur.rahman.cs@gmail.com>
2026-06-02 18:14:09 +0600

    Add Home Decor category page with metadata generation

---
+++ src/app/(public)/products/home-decor/page.tsx
@@ -0,0 +1,5 @@
+import { createCategoryMetadata, createCategoryPage } from "../categoryPage";
+
+export const generateMetadata = createCategoryMetadata("HOME_DECOR");
+
+export default createCategoryPage("HOME_DECOR");
