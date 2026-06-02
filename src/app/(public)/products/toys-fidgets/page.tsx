6a2cdcc8d3166e94729b85f6f3c55fd43e1a081f
Nahidur Rahman <nahidur.rahman.cs@gmail.com>
2026-06-02 18:14:30 +0600

    Add Toys and Fidgets category page with metadata generation

---
+++ src/app/(public)/products/toys-fidgets/page.tsx
@@ -0,0 +1,5 @@
+import { createCategoryMetadata, createCategoryPage } from "../categoryPage";
+
+export const generateMetadata = createCategoryMetadata("TOYS_AND_FIDGETS");
+
+export default createCategoryPage("TOYS_AND_FIDGETS");
