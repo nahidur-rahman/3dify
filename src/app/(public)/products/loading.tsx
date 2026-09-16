303ae06e4a4dc07ce7fc0a50f8fa1d2ed5106ef1
Nahidur Rahman <nahidur.rahman.cs@gmail.com>
2026-09-16 16:16:59 +0600

    feat(loading): add product count to StorefrontProductGridSkeleton

--- src/app/(public)/products/loading.tsx
+++ src/app/(public)/products/loading.tsx
@@ -1,11 +1,12 @@
 import StorefrontProductGridSkeleton from "@/components/loading/StorefrontProductGridSkeleton";
 import ProductCatalogFilterSkeleton from "@/components/loading/ProductCatalogFilterSkeleton";
+import { PRODUCT_CATALOG_PAGE_SIZE } from "@/lib/productCatalogConfig";
 
 export default function ProductsLoading() {
   return (
     <div className="mx-auto max-w-7xl px-3 py-4 sm:px-6 sm:py-5 lg:px-8">
       <ProductCatalogFilterSkeleton />
-      <StorefrontProductGridSkeleton />
+      <StorefrontProductGridSkeleton count={PRODUCT_CATALOG_PAGE_SIZE} />
     </div>
   );
 }
